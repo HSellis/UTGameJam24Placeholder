9d96d467c5632bfa2f7b93ffcd5b684c1b2b279d
HSellis <henri.sellis@gmail.com>
2024-10-05 22:09:14 +0300

    Scale bag well

--- Assets/Custom/Scripts/Player.cs
+++ Assets/Custom/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     public static Player Instance { get; private set; }
 
+    private List<Present> collectedPresents = new List<Present> ();
+    public float bagSize = 1.0f;
+
     private RandomAudioPlayer randomAudioPlayer;
     public AudioClip[] damageAudioClips;
 
@@ -35,15 +38,25 @@ public class Player : MonoBehaviour
         Elf elf = other.gameObject.GetComponent<Elf>();
         if (elf != null)
         {
-            ScaleBag(0.9f);
+            bagSize -= 0.1f;
+            Debug.Log(bagSize);
+            ScaleBag(bagSize);
             randomAudioPlayer.PlayRandomClip(damageAudioClips);
+
+            if (bagSize < 0.5f)
+            {
+                Debug.Log("You lost!");
+            }
         }
 
         Present present = other.gameObject.GetComponent<Present>();
-        if (present != null)
+        if (present != null && !collectedPresents.Contains(present))
         {
             present.CollectPresent();
-            ScaleBag(1.1f);
+            collectedPresents.Add(present);
+            bagSize += 0.1f;
+            Debug.Log(bagSize);
+            ScaleBag(bagSize);
 
             spawner.SpawnPresentsAndElves();
         }
@@ -51,6 +64,6 @@ public class Player : MonoBehaviour
 
     private void ScaleBag(float scale)
     {
-        bag.transform.localScale = Vector3.Scale(bag.transform.localScale, new Vector3(scale, scale, scale));
+        bag.transform.localScale = new Vector3(scale, scale, scale);
     }
 }
