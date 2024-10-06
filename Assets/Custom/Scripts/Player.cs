1691b9de285943db260a1d6d06236af387de8204
HSellis <henri.sellis@gmail.com>
2024-10-06 11:10:00 +0300

    Add Speed Powerup logic

--- Assets/Custom/Scripts/Player.cs
+++ Assets/Custom/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,14 @@ public class Player : MonoBehaviour
     private List<Present> collectedPresents = new List<Present> ();
     public int collectedPresentsNum = 0;
 
+    private string appliedPowerUpType;
+    public float defaultSprintSpeed = 5.25f;
+    public float poweredSprintSpeed = 8f;
+
+    public Transform powerUpTransform;
+
     private RandomAudioPlayer randomAudioPlayer;
+    private ThirdPersonController thirdPersonController;
     public AudioClip[] damageAudioClips;
 
     public GameObject bag;
@@ -27,6 +35,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         randomAudioPlayer = GetComponent<RandomAudioPlayer>();
+        thirdPersonController = GetComponent<ThirdPersonController>();
     }
 
     // Update is called once per frame
@@ -54,11 +63,17 @@ public class Player : MonoBehaviour
         Present present = other.gameObject.GetComponent<Present>();
         if (present != null && !collectedPresents.Contains(present))
         {
+            appliedPowerUpType = present.powerUpType;
+            float powerUpDuration = present.powerUpDuration;
+            GameObject powerUpIcon = present.powerUpIcon;
+
             present.CollectPresent();
             collectedPresents.Add(present);
             collectedPresentsNum += 1;
             ScaleBag(1 + collectedPresentsNum * 0.1f);
             gameMenu.UpdatePresentsText(collectedPresentsNum);
+
+            ApplyPowerUp(appliedPowerUpType, powerUpIcon, powerUpDuration);
 
             spawner.SpawnPresentsAndElves();
         }
@@ -67,5 +82,27 @@ public class Player : MonoBehaviour
     private void ScaleBag(float scale)
     {
         bag.transform.localScale = Vector3.one * scale;
+    }
+
+    private void ApplyPowerUp(string powerUpType, GameObject powerUpIcon, float powerUpDuration)
+    {
+        if (powerUpType == "SPEED")
+        {
+            thirdPersonController.SprintSpeed = poweredSprintSpeed;
+        }
+
+        Instantiate(powerUpIcon, powerUpTransform.position, Quaternion.identity, powerUpTransform);
+        Invoke("EndPowerUp", powerUpDuration);
+    }
+
+    void EndPowerUp()
+    {
+        if (appliedPowerUpType == "SPEED")
+        {
+            thirdPersonController.SprintSpeed = defaultSprintSpeed;
+        }
+
+        Destroy(powerUpTransform.GetChild(0).gameObject);
+        appliedPowerUpType = null;
     }
 }
--- Assets/Custom/Scripts/PowerUp.cs
+++ Assets/Custom/Scripts/PowerUp.cs
@@ -2,27 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Present : MonoBehaviour
+public class PowerUp : MonoBehaviour
 {
+    public Transform rotatingPartTrans;
+
     public float rotationSpeed = 50f; // Speed of rotation
     public float floatSpeed = 2f;     // Speed of floating up and down
     public float floatHeight = 0.5f;  // How high/low the object floats
-
     private Vector3 startPosition;
-    private RandomAudioPlayer randomAudioPlayer;
-    public Transform rotatingPartTrans;
-
-    public AudioClip[] presentOpenClips;
-
-    public string powerUpType = "SPEED";
-    public float powerUpDuration = 5f;
-    public GameObject powerUpIcon;
 
     // Start is called before the first frame update
     void Start()
     {
         startPosition = rotatingPartTrans.localPosition;
-        randomAudioPlayer = GetComponent<RandomAudioPlayer>();
     }
 
     // Update is called once per frame
@@ -38,7 +30,7 @@ public class Present : MonoBehaviour
     void RotateObject()
     {
         // Rotate the object based on rotation speed and time
-        rotatingPartTrans.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        rotatingPartTrans.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
     }
 
     void FloatObject()
@@ -46,11 +38,5 @@ public class Present : MonoBehaviour
         // Create a smooth up and down motion using a sine wave
         float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
         rotatingPartTrans.localPosition = new Vector3(rotatingPartTrans.localPosition.x, newY, rotatingPartTrans.localPosition.z);
-    }
-
-    public void CollectPresent()
-    {
-        randomAudioPlayer.PlayRandomClip(presentOpenClips);
-        Destroy(gameObject, 0.5f);
     }
 }
